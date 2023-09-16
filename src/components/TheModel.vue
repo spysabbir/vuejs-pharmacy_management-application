3dc1decc37c6779f43bd056044267c62570c46d0
spysabbir <sabbirahammedsovon@gmail.com>
2023-09-16 23:56:49 +0600

    Supplier setup

---
+++ src/components/TheModel.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="modal fade bd-example-modal-lg" tabindex="-1" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">{{ hadding }}</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="btn-close"></button>
+        </div>
+        <div class="modal-body">
+            <slot></slot>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+        hadding: {
+            type: String,
+            default: "Default Heading"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/dashboard/Supplier.vue
+++ src/views/dashboard/Supplier.vue
@@ -1,13 +1,99 @@
 <template>
     <TheBreadcrumb title="Supplier"></TheBreadcrumb>
+
+  <div class="row">
+    <div class="col-12 col-xl-12 grid-margin stretch-card">
+      <div class="card overflow-hidden">
+        <div class="card-body">
+          <div class="d-flex justify-content-between align-items-baseline mb-4 mb-md-3">
+            <h6 class="card-title mb-0">Supplier</h6>
+          </div>
+          <div class="row align-items-start">
+            <div class="col-md-7">
+              <p class="text-muted tx-13 mb-3 mb-md-0">Revenue is the income that a business has from its normal business activities, usually from the sale of goods and services to customers.</p>
+            </div>
+            <div class="col-md-5 d-flex justify-content-md-end">
+              <TheButton data-bs-toggle="modal" data-bs-target=".bd-example-modal-lg">Add New</TheButton>
+            </div>
+          </div>
+          <div></div>
+        </div>
+        <div class="card-body">
+          <div class="table-responsive">
+            <table class="table table-striped table-hover align-middle">
+              <thead class="table-light">
+                <caption>Table Name</caption>
+                <tr>
+                  <th>Column 1</th>
+                  <th>Column 2</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="i in 9" :key="i">
+                  <td>Item</td>
+                  <td>Item</td>
+                  <td>
+
+                  </td>
+                </tr>
+              </tbody>
+              <tfoot>
+                
+              </tfoot>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <TheModel hadding="Add Supplier">
+    <form @submit.prevent="addSupplier">
+      <div class="mb-3">
+        <label class="form-label">Name</label>
+        <input type="text" class="form-control" v-model="supplierData.name" placeholder="Enter name">
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Email</label>
+        <input type="email" class="form-control" v-model="supplierData.email" placeholder="Enter email">
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Phone Number</label>
+        <input type="text" class="form-control" v-model="supplierData.phoneNumber" placeholder="Enter phone number">
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Address</label>
+        <textarea class="form-control" v-model="supplierData.address" placeholder="Enter address"></textarea>
+      </div>
+      <TheButton>Add Supplier</TheButton>
+    </form>
+  </TheModel>
 </template>
 
 <script>
 import TheBreadcrumb from '../../components/TheBreadcrumb.vue';
+import TheButton from '../../components/TheButton.vue';
+import TheModel from '../../components/TheModel.vue';
 
 export default {
-components: {
+  data: () => ({
+    supplierData: {
+      name: "",
+      email: "",
+      phoneNumber: "",
+      address: "",
+    }
+  }),
+  components: {
     TheBreadcrumb,
+    TheButton,
+    TheModel,
+  },
+  methods: {
+    addNew(){
+
+    }
   }
 }
 </script>
