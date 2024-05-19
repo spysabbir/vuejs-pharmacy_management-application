f097e78b92e0213ca33b2e575e27ccef1821e13d
spysabbir <sabbirahammedsovon@gmail.com>
2024-05-19 12:12:06 +0600

    Medicine list datatable system added

--- src/views/dashboard/Medicine.vue
+++ src/views/dashboard/Medicine.vue
@@ -195,11 +195,33 @@ const deleteMedicine = () => {
       deletingStatus.value = false;
     });
 };
+
+const searchField = ["name"];
+const searchValue = ref();
+
+const headers = [
+  { text: "Supplier Name", value: "supplier_name" },
+  { text: "Type Name", value: "type_name" },
+  { text: "Medicine Name", value: "name" },
+  { text: "Power Name", value: "power_name" },
+  { text: "Unit Name", value: "unit_name" },
+  { text: "Rack Name", value: "rack_name" },
+  { text: "Purchases Quantity", value: "purchases_quantity" },
+  { text: "Sales Quantity", value: "sales_quantity" },
+  { text: "Purchases Price", value: "purchases_price" },
+  { text: "Sales Price", value: "sales_price" },
+  { text: "Stock Quantity", value: "stock_quantity" },
+  { text: "Created At", value: "created_at", sortable: true },
+  { text: "Action", value: "action" },
+];
+
+const selectedMedicineId = (id) => {
+  selectedMedicineData.value = medicines.value.find((item) => item.id === id);
+};
 </script>
 
 <template>
   <TheBreadcrumb title="Medicine"></TheBreadcrumb>
-
   <!--begin::Tables-->
   <div class="card mb-5 mb-xl-8">
     <!--begin::Header-->
@@ -226,92 +248,38 @@ const deleteMedicine = () => {
     <div class="card-body py-3">
       <!--begin::Table container-->
       <div class="text-center" v-if="!getMedicines">Loading...</div>
-      <div class="table-responsive" v-else>
-        <!--begin::Table-->
-        <table class="table table-row-dashed table-row-gray-300 align-middle gs-0 gy-4">
-          <!--begin::Table head-->
-          <thead>
-            <tr class="fw-bolder text-muted">
-              <th class="min-w-150px">Sl No</th>
-              <th class="min-w-150px">Supplier Name</th>
-              <th class="min-w-150px">Medicine Details</th>
-              <th class="min-w-150px">Rack</th>
-              <th class="min-w-150px">Purchases Price</th>
-              <th class="min-w-100px text-end">Actions</th>
-            </tr>
-          </thead>
-          <!--end::Table head-->
-          <!--begin::Table body-->
-          <tbody>
-            <template v-if="medicines.length >= 1">
-              <tr v-for="(medicine, i) in medicines" :key="medicine.name">
-                <td>{{ i+1 }}</td>
-                <td>
-                  <div class="d-flex align-items-center">
-                    <div class="d-flex justify-content-start flex-column">
-                      <p class="text-dark fw-bolder text-hover-primary fs-6">{{ medicine.supplier_name }}</p>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  <div class="d-flex align-items-center">
-                    <div class="d-flex justify-content-start flex-column">
-                      <p class="text-dark fw-bolder text-hover-primary mb-1">{{ medicine.type_name }}</p>
-                      <strong class="text-info fw-bold d-block text-hover-primary">{{ medicine.name }} | {{ medicine.power_name }}</strong>
-                      <p class="text-dark fw-bolder text-hover-primary">{{ medicine.unit_name }}</p>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  <div class="d-flex align-items-center">
-                    <div class="d-flex justify-content-start flex-column">
-                      <p class="text-dark fw-bolder text-hover-primary fs-6">{{ medicine.rack_name }}</p>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  <div class="d-flex align-items-center">
-                    <div class="d-flex justify-content-start flex-column">
-                      <p class="text-dark fw-bolder text-hover-primary fs-6">{{ medicine.purchases_price }}</p>
-                    </div>
-                  </div>
-                </td>
-                <td class="text-end">
-                  <TheButton data-bs-toggle="modal" data-bs-target=".editingModel" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1" color="success" @click="selectedMedicineData = medicine">
-                    <!--begin::Svg Icon | path: icons/duotone/Communication/Write.svg-->
-                    <span class="svg-icon svg-icon-3">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                        <path d="M12.2674799,18.2323597 L12.0084872,5.45852451 C12.0004303,5.06114792 12.1504154,4.6768183 12.4255037,4.38993949 L15.0030167,1.70195304 L17.5910752,4.40093695 C17.8599071,4.6812911 18.0095067,5.05499603 18.0083938,5.44341307 L17.9718262,18.2062508 C17.9694575,19.0329966 17.2985816,19.701953 16.4718324,19.701953 L13.7671717,19.701953 C12.9505952,19.701953 12.2840328,19.0487684 12.2674799,18.2323597 Z" fill="#000000" fill-rule="nonzero" transform="translate(14.701953, 10.701953) rotate(-135.000000) translate(-14.701953, -10.701953)" />
-                        <path d="M12.9,2 C13.4522847,2 13.9,2.44771525 13.9,3 C13.9,3.55228475 13.4522847,4 12.9,4 L6,4 C4.8954305,4 4,4.8954305 4,6 L4,18 C4,19.1045695 4.8954305,20 6,20 L18,20 C19.1045695,20 20,19.1045695 20,18 L20,13 C20,12.4477153 20.4477153,12 21,12 C21.5522847,12 22,12.4477153 22,13 L22,18 C22,20.209139 20.209139,22 18,22 L6,22 C3.790861,22 2,20.209139 2,18 L2,6 C2,3.790861 3.790861,2 6,2 L12.9,2 Z" fill="#000000" fill-rule="nonzero" opacity="0.3" />
-                      </svg>
-                    </span>
-                    <!--end::Svg Icon-->
-                </TheButton>
-                <TheButton data-bs-toggle="modal" data-bs-target=".deletingModel" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm" color="warning" @click="selectedMedicineData = medicine">
-                    <!--begin::Svg Icon | path: icons/duotone/General/Trash.svg-->
-                    <span class="svg-icon svg-icon-3">
-                      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                          <rect x="0" y="0" width="24" height="24" />
-                          <path d="M6,8 L6,20.5 C6,21.3284271 6.67157288,22 7.5,22 L16.5,22 C17.3284271,22 18,21.3284271 18,20.5 L18,8 L6,8 Z" fill="#000000" fill-rule="nonzero" />
-                          <path d="M14,4.5 L14,4 C14,3.44771525 13.5522847,3 13,3 L11,3 C10.4477153,3 10,3.44771525 10,4 L10,4.5 L5.5,4.5 C5.22385763,4.5 5,4.72385763 5,5 L5,5.5 C5,5.77614237 5.22385763,6 5.5,6 L18.5,6 C18.7761424,6 19,5.77614237 19,5.5 L19,5 C19,4.72385763 18.7761424,4.5 18.5,4.5 L14,4.5 Z" fill="#000000" opacity="0.3" />
-                        </g>
-                      </svg>
-                    </span>
-                    <!--end::Svg Icon-->
-                </TheButton>
-                </td>
-              </tr>
-            </template>
-            <tr v-else>
-              <td colspan="6" class="text-center">
-                <span class="text-danger">Medicine not found!</span>
-              </td>
-            </tr>
-          </tbody>
-          <!--end::Table body-->
-        </table>
-        <!--end::Table-->
+      <div v-else>
+        <input placeholder="Search..." class="form-control mb-3 w-auto" type="text" v-model="searchValue"/>
+        <EasyDataTable buttons-pagination border-cell alternating show-index header-text-direction="center" body-text-direction="center" :headers="headers" :items="medicines" :rows-per-page="10" :search-field="searchField" :search-value="searchValue">
+          <template #item-stock_quantity="{ purchases_quantity, sales_quantity }">
+            <span class="badge badge-info">{{ purchases_quantity- sales_quantity }}</span>
+          </template>
+          <template #item-action="{ id }">
+            <TheButton data-bs-toggle="modal" data-bs-target=".editingModel" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1" color="success" @click="selectedMedicineId(id)">
+              <!--begin::Svg Icon | path: icons/duotone/Communication/Write.svg-->
+              <span class="svg-icon svg-icon-3">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                  <path d="M12.2674799,18.2323597 L12.0084872,5.45852451 C12.0004303,5.06114792 12.1504154,4.6768183 12.4255037,4.38993949 L15.0030167,1.70195304 L17.5910752,4.40093695 C17.8599071,4.6812911 18.0095067,5.05499603 18.0083938,5.44341307 L17.9718262,18.2062508 C17.9694575,19.0329966 17.2985816,19.701953 16.4718324,19.701953 L13.7671717,19.701953 C12.9505952,19.701953 12.2840328,19.0487684 12.2674799,18.2323597 Z" fill="#000000" fill-rule="nonzero" transform="translate(14.701953, 10.701953) rotate(-135.000000) translate(-14.701953, -10.701953)" />
+                  <path d="M12.9,2 C13.4522847,2 13.9,2.44771525 13.9,3 C13.9,3.55228475 13.4522847,4 12.9,4 L6,4 C4.8954305,4 4,4.8954305 4,6 L4,18 C4,19.1045695 4.8954305,20 6,20 L18,20 C19.1045695,20 20,19.1045695 20,18 L20,13 C20,12.4477153 20.4477153,12 21,12 C21.5522847,12 22,12.4477153 22,13 L22,18 C22,20.209139 20.209139,22 18,22 L6,22 C3.790861,22 2,20.209139 2,18 L2,6 C2,3.790861 3.790861,2 6,2 L12.9,2 Z" fill="#000000" fill-rule="nonzero" opacity="0.3" />
+                </svg>
+              </span>
+              <!--end::Svg Icon-->
+            </TheButton>
+            <TheButton data-bs-toggle="modal" data-bs-target=".deletingModel" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm" color="warning" @click="selectedMedicineId(id)">
+              <!--begin::Svg Icon | path: icons/duotone/General/Trash.svg-->
+              <span class="svg-icon svg-icon-3">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                  <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                    <rect x="0" y="0" width="24" height="24" />
+                    <path d="M6,8 L6,20.5 C6,21.3284271 6.67157288,22 7.5,22 L16.5,22 C17.3284271,22 18,21.3284271 18,20.5 L18,8 L6,8 Z" fill="#000000" fill-rule="nonzero" />
+                    <path d="M14,4.5 L14,4 C14,3.44771525 13.5522847,3 13,3 L11,3 C10.4477153,3 10,3.44771525 10,4 L10,4.5 L5.5,4.5 C5.22385763,4.5 5,4.72385763 5,5 L5,5.5 C5,5.77614237 5.22385763,6 5.5,6 L18.5,6 C18.7761424,6 19,5.77614237 19,5.5 L19,5 C19,4.72385763 18.7761424,4.5 18.5,4.5 L14,4.5 Z" fill="#000000" opacity="0.3" />
+                  </g>
+                </svg>
+              </span>
+              <!--end::Svg Icon-->
+            </TheButton>
+          </template>
+        </EasyDataTable>
       </div>
       <!--end::Table container-->
     </div>
