bf4f34273aa373001fe11b39034f4338270bc995
spysabbir <sabbirahammedsovon@gmail.com>
2024-05-25 17:14:16 +0600

    Home page design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,22 @@
+<template>
+<div class="d-flex flex-column min-vh-100 bg-light">
+    <header class="flex-grow-1 d-flex align-items-center justify-content-center">
+        <div class="card shadow-lg p-5 text-center">
+            <h1 class="mb-2">
+                Pharmacy Management Application
+            </h1>
+            <p class="text-muted mb-4">Manage your pharmacy effortlessly with our comprehensive solution.</p>
+            <router-link to="/login" class="btn btn-primary w-100 mb-3">
+                Get Started
+            </router-link>
+        </div>
+    </header>
+</div>
+</template>
+
 <script setup>
 
 </script>
 
-<template>
-    <router-link to="/login">Login</router-link>
-</template>
+<style scoped>
+</style>
